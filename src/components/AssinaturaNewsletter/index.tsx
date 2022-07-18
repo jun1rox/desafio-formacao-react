40b0f366161ee24b366ea33a279b2889bf53e713
jun1rox <junior_alaniz@hotmail.com>
2022-07-18 17:16:00 -0300

    dia 3

--- src/components/AssinaturaNewsletter/index.tsx
+++ src/components/AssinaturaNewsletter/index.tsx
@@ -1,5 +1,49 @@
+import { useState } from 'react';
+import styles from './AssinaturaNewsletter.module.scss';
+
 export default function AssinaturaNewsletter() {
+
+  const [emailValido, setEmailValido] = useState(false);
+  const [email, setEmail] = useState('');
+
+  function validarEmail(email: string) {
+    if (/.+@.+\..+/.test(email)) {
+      setEmailValido(true);
+    } else {
+      setEmailValido(false);
+    }
+
+    setEmail(email);
+  }
+
+  function aoSubmeter(evento: React.FormEvent<HTMLFormElement>) {
+    evento.preventDefault();
+    console.log('clickei');
+    alert(`Obrigado pela sua assinatura, você receberá nossas novidades no e-mail ${email}.`);
+
+  };
+
   return (
-    <></>
+    <div className={styles.newsletter}>
+      <div className={styles.texto}>
+        <p className={styles.titulo}>Sua casa com as</p>
+        <p className={styles.destaque}>melhores plantas</p>
+        <p className={styles.subtitulo}>Encontre aqui uma vasta seleção de plantas para decorar a sua casa e torná-lo uma pessoa mais feliz no seu dia a dia. Entre com seu e-mail e assine nossa newsletter para saber das novidades da marca.</p>
+      </div>
+      <form
+        className={styles.formulario}
+        onSubmit={aoSubmeter}
+      >
+        <input
+          type='text'
+          placeholder='Insira seu email'
+          value={email}
+          onChange={(event) => validarEmail(event.target.value)}
+        />
+        <button disabled={!emailValido} type='submit'>
+          Assinar newsletter
+        </button>
+      </form>
+    </div>
   );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,20 @@
 import Menu from 'components/Menu';
 import AssinaturaNewsletter from 'components/AssinaturaNewsletter';
+
 import { ReactComponent as Forma } from 'assets/forma.svg';
+import { ReactComponent as Planta } from 'assets/planta.svg';
+
 import styles from './Home.module.scss';
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.container}>
       <Forma className={styles.forma} />
       <Menu />
-      <AssinaturaNewsletter />
+      <div className={styles.newsletter__container}>
+        <AssinaturaNewsletter />
+        <Planta className={styles.planta} />
+      </div>
     </main>
   );
 }
